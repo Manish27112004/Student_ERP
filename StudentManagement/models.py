712980302ba30083acb137069c9ce2da3fdef7bc
Manish27112004 <manish27112004@gmail.com>
2024-09-10 00:34:35 +0530

    Updation_9/10/24

--- StudentManagement/models.py
+++ StudentManagement/models.py
@@ -23,23 +23,29 @@ class Student(models.Model):
 
 class Announcement(models.Model):
     announcement = models.CharField(max_length= 255)
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add= True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='announcements')
     
 
 class Calender(models.Model):
     event = models.CharField(max_length= 255)
     eventTime = models.DateTimeField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='events')
+    students = models.ManyToManyField(Student, related_name='events_viewable')
 
 
-class SharingStore(models.Model):
+class SharingIsCaringStore(models.Model):
     price = models.SmallIntegerField()
     bookName = models.CharField(max_length= 255)
     image = models.ImageField(upload_to='images/')  # Customize the upload path...This argument specifies the directory within your media root where uploaded images will be saved. You'll need to configure your media settings in settings.py
+    seller = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='books_sold')
+    buyer = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='books_bought', null=True, blank=True)
 
 
 class Stationary(models.Model):
     itemName = models.CharField(max_length= 255)
     price = models.DecimalField(max_digits= 6, decimal_places= 2)
+    buyer = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='books_bought_from_stationary', null=True, blank=True) #It should return all the books that are being bought from the stationary by a particular student
 
 
 class ToDoList(models.Model):
